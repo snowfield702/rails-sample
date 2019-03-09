41a29271521cf93acbb1d89348c2002d4f243594
snowfield702 <snowfield702@gmail.com>
2019-03-09 22:59:25 +0900

    CircleCIで実行したrubocopの結果をTest Summeryに表示

--- .circleci/run-rubocop.sh
+++ .circleci/run-rubocop.sh
@@ -3,4 +3,8 @@ set -v
 
 export RUBYOPT=-EUTF-8
 
-bundle exec rubocop
+bundle exec rubocop \
+  -r $(bundle show rubocop-junit-formatter)/lib/rubocop/formatter/junit_formatter.rb \
+  -c .rubocop.yml \
+  --format RuboCop::Formatter::JUnitFormatter \
+  --out $CIRCLE_TEST_REPORTS/rubocop/rubocop.xml
